81eebd58b3b5a9ddfa11646fd63ca9771ee84d1e
CristianoCruz10 <cruzcristianno@gmail.com>
2023-09-01 13:45:54 -0300

    aula2

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,9 +1,15 @@
+import java.sql.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
        String nome = "Cristiano";   
-    System.out.println(nome);
-        final int numero = 5;
+        int idade = 24;
+        double peso = 85.9;
+        char sexo = 'M';
+        boolean doadorSangue = true;   
+       
         
-        System.out.println(numero);
+        double soma = 20.6 + 45.9;
+            System.out.println(soma);
     }
 }
