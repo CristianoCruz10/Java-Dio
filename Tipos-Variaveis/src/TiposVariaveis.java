31c21d7adf236864f304cc4179f570cd53292366
CristianoCruz10 <cruzcristianno@gmail.com>
2023-09-01 13:12:29 -0300

    testando o vscode

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        system.out.println(salarioMinimo);
+            
     }
 }
