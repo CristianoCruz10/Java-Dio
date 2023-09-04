a7b2248221b933906d80ddad6a43858708063ccb
CristianoCruz10 <cruzcristianno@gmail.com>
2023-09-04 18:04:26 -0300

    Operadores Boolean

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,15 +1,27 @@
-import java.sql.Date;
-
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-       String nome = "Cristiano";   
-        int idade = 24;
-        double peso = 85.9;
-        char sexo = 'M';
-        boolean doadorSangue = true;   
-       
-        
-        double soma = 20.6 + 45.9;
-            System.out.println(soma);
+    
+ int numero1 = 1;
+ int numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+
+    if (numero1 < numero2) {
+        System.out.println("Essa condição é verdadeira");
+    }else
+        System.out.println("Essa condição é falsa");
+
+    
+    System.out.println("A variavel numeroUm é igual a numerodois? " + simNao);
+
+    simNao = numero1 != numero2;
+        System.out.println("A variavel numeroUm é diferente da variavel numeroDois? " + simNao);
+
+    simNao = numero1 > numero2;
+        System.out.println("A variavel numeroUm é maior que a numeroDois? " +simNao);
+
+    simNao = numero1 < numero2;
+        System.out.println("A variavel numeroUm é menor que a numeroDois? " +simNao);
+
     }
 }
