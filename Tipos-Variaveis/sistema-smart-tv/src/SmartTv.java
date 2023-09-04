e9a471a684e6dd6edb1248565cff896fe584cef5
CristianoCruz10 <cruzcristianno@gmail.com>
2023-09-04 19:42:32 -0300

    Projeto de SmartTv

--- Tipos-Variaveis/sistema-smart-tv/src/SmartTv.java
+++ Tipos-Variaveis/sistema-smart-tv/src/SmartTv.java
@@ -5,14 +5,14 @@ int canal = 1 ;
 int volume = 25;
 
     public void mudarCanal(int novoCanal){
-    canal = novoCanal;
+        canal = novoCanal;
 }
 
     public void aumentarCanal(){
         canal++;
 }
     public void diminuirCanal(){
-    canal--;
+        canal--;
 }
 
     public void aumentarVolume(){
