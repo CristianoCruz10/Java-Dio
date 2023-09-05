f45b8a18cd3b4c11fca3d0297c7b25bdf94615a5
CristianoCruz10 <cruzcristianno@gmail.com>
2023-09-05 13:14:41 -0300

    meu banco pelo terminal

--- Java-Terminal/Conta-Bancaria/src/ContaTerminal.java
+++ Java-Terminal/Conta-Bancaria/src/ContaTerminal.java
@@ -1,10 +1,50 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args){
- Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    private int agencia;
+    private int conta;
+    private double saldo;
 
-       
+    public ContaTerminal(int agencia, int conta) {
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = 0; // saldo da conta, se inicia com zero
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$ " + valor + " Realizado com sucesso.");
+        } else {
+            System.out.println("Valor depositado inválido.");
+        }
+    }
+
+    public void mostrarSaldo() {
+        System.out.println("O saldo da conta " + agencia + " / " + conta + ": R$" + saldo);
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Deseja realizar um saque de R$" + valor + " ? (sim ou não): ");
+            String resposta = scanner.nextLine().toLowerCase(); // ler a resposta do usuário e converter para minúsculas
+
+            if (resposta.equals("sim")) {
+                if (valor <= saldo) {
+                    saldo -= valor;
+                    System.out.println("Saque de R$" + valor + " Realizado com sucesso.");
+                } else {
+                    System.out.println("Saldo insuficiente para realizar o saque!");
+                }
+            } else if (resposta.equals("não")) {
+                System.out.println("Saque cancelado.");
+            } else {
+                System.out.println("Resposta inválida. Nenhum saque foi realizado.");
+            }
+        } else {
+            System.out.println("Valor do saque inválido!");
+        }
     }
 }
+
---
+++ Java-Terminal/Conta-Bancaria/src/UsuarioBanco.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class UsuarioBanco {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite o numero da sua agencia bancaria: ");
+    int agencia = scanner.nextInt();
+
+    System.out.println("Digite o numero da sua conta bancaria: ");
+    int conta = scanner.nextInt();
+
+    System.out.println("Bem Vindo a sua conta bancaria");
+
+    ContaTerminal minhaConta = new ContaTerminal(agencia, conta);
+
+    System.out.println("Digite o valor que deseja depositar: ");
+    double depositar = scanner.nextDouble();
+
+    minhaConta.depositar(depositar);
+    minhaConta.mostrarSaldo();
+
+    System.out.println("Digite o valor que deseja sacar: ");
+    double sacarValor = scanner.nextDouble();
+    minhaConta.sacar(sacarValor);
+    minhaConta.mostrarSaldo();
+
+    scanner.close();
+        
+    }
+}
